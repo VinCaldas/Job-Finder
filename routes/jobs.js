a3adeb4803f578e278a9634bd44d2957ebb8fbd2
Vitor Oliveira <vinicius.caldas0708@gmail.com>
2023-01-10 10:50:18 -0300

    initial commit

---
+++ routes/jobs.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../models/Job');
+
+router.get('/test', (req, res) => {
+    res.send('deu certo');
+})
+
+//detalhe da vaga
+router.get('/view/:id', (req, res) => Job.findOne({
+        where: {id: req.params.id}
+    }).then(job => {
+        res.render('view', {  
+            job
+    })
+
+    }).catch(err => console.error(err))
+)
+
+
+
+router.get('/add', (req, res) => {
+    res.render('add');
+})
+
+router.post('/add', (req, res,) => {
+    let { title, salary, company, description, email, new_job } = req.body;
+
+    Job.create({
+        title,
+        salary,
+        company,
+        description,
+        email,
+        new_job
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err));
+})
+
+
+
+module.exports = router;
